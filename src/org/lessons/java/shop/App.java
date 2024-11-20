86de095c2697e985fe4fb247eb34a5bd35db30ec
AmerigoBovo97 <amerigobovo@gamil.com>
2024-11-20 21:45:10 +0100

    feat: add debug and main function

--- src/org/lessons/java/shop/App.java
+++ src/org/lessons/java/shop/App.java
@@ -2,4 +2,9 @@ package org.lessons.java.shop;
 
 public class App {
     
+    public void main(){
+        Prodotto product1 = new Prodotto("Pupazzo di Cane Pazzo", "è un semplice pupazzo di cane ma con gli occhi cuciti male", 15.50, 22);
+        System.out.println(product1.codice + product1.descrizione + product1.iva);
+
+    }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -5,17 +5,17 @@ public class Prodotto {
     int codice;
     String nome;
     String descrizione;
-    float prezzo;
+    double prezzo;
     int iva;
 
 
-    public Prodotto(String nome, String descrizione, float prezzo, int iva){
+    public Prodotto(String nome, String descrizione, double prezzo, int iva){
         
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
         this.iva = iva;
-        
+
     }
 
 }
