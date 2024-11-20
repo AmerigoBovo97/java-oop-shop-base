b03331a21a310f40662c8a5de7805898bceb9d6f
AmerigoBovo97 <amerigobovo@gamil.com>
2024-11-20 21:37:39 +0100

    feat: add Prodotto constructor and fields

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -2,4 +2,20 @@ package org.lessons.java.shop;
 
 public class Prodotto {
     
+    int codice;
+    String nome;
+    String descrizione;
+    float prezzo;
+    int iva;
+
+
+    public Prodotto(String nome, String descrizione, float prezzo, int iva){
+        
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+        
+    }
+
 }
